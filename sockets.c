53b8d2917c1ff6cd566490d9388cb3d1d2ed8f1f
Ricardo Moureira <111253681+ricardopzzn@users.noreply.github.com>
2025-04-08 18:13:25 -0400

    Atualizando args, e numero de portas TCP para analise

--- sockets.c
+++ sockets.c
@@ -1,34 +1,52 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
-int main(void){
+int main(int argc, char *argv[]){
+
+
+	if (argc < 2) {
+		printf("Uso: %s <IP> \n", argv[0]);
+		return 1;
+
+	}
 
 	int meusocket;
 	int conecta;
 
+	int porta;
+	int inicio = 0;
+	int final = 65535;
+	char *destino = argv[1];
+
+
 	struct sockaddr_in alvo;
 
-	meusocket = socket(AF_INET, SOCK_STREAM, 0);
-	alvo.sin_family = AF_INET;
-	alvo.sin_port = htons(80);
-	alvo.sin_addr.s_addr = inet_addr("37.59.174.225");
 
-	conecta = connect(meusocket, (struct sockaddr *)&alvo, sizeof alvo);
-	
-	if(conecta == 0){
+        for(porta=inicio;porta<final;porta++){
+		meusocket = socket(AF_INET, SOCK_STREAM, 0);
 
-		printf("Porta Aberta \n");
-		close(meusocket);
-		close(conecta);
-	
-	} else {
-		printf("Porta Fechada \n");
+		alvo.sin_family = AF_INET;
+		alvo.sin_port = htons(porta);
+		alvo.sin_addr.s_addr = inet_addr(destino);
 
+		conecta = connect(meusocket, (struct sockaddr *)&alvo, sizeof alvo);
+
+		if(conecta == 0){
+
+			printf("Porta %d - status [ABERTA] \n",porta);
+			close(meusocket);
+			close(conecta);
+
+		} else {
+			close(meusocket);
+
+
+		}
 	}
 
-
+	return 0;
 
 }
